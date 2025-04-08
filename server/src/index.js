376ae2e706d7c87d242309c639d8bc4073d21616
Your Name <you@example.com>
2025-04-08 10:31:08 +0530

    adding rec app frontend and backend

---
+++ server/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user.js";
+import { recipesRouter } from "./routes/recipes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/recipes", recipesRouter);
+
+mongoose.connect(
+  "mongodb+srv://sujithpatel25:Sujithpatel123@receipesharing.e9g5djr.mongodb.net/receipe_db?retryWrites=true&w=majority&appName=receipesharing",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(
+    console.log("DB connected...")
+  )
+
+app.listen(3001, () => console.log("Server started"));
